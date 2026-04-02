134ceaf88840cc5aa1c3b16fa77813d7762a4b52
tjasz <nukorausa@hotmail.com>
2026-04-02 08:36:00 -0700

    Use HashRouter for pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import RidesPage from './RidesPage';
 import './index.css';
 
-const basename = import.meta.env.BASE_URL || '/';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/rides" element={<RidesPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
